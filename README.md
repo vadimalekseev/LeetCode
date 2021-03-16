8b9d919bb4172a0ccf6a7b2044beba2697e0e065
Vadim Alekseev <Vad1mAlekseev2807@gmail.com>
2021-03-16 16:49:08 +0300

    Add build script

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
-# LeetCodeProblemSolving
+# This repo contains my solvings leetcode problems
+
+This file was generated automatically by [buildMdFile.js](https://github.com/Vad1mAlekseev/LeetCodeProblemSolving/blob/main/scripts/buildMdFile.js) script.
+
+Count of all problems solved: 0
+
+
+
+<details>
+<summary>All solvings</summary>
+
+| #     | Problem            | Difficulty | Solutions       |
+|:-----:|:------------------:|:----------:|:---------------:|
+
+
+</details>
+<details>
+<summary>Solvings with difficulty "Hard"</summary>
+
+| #     | Problem            | Difficulty | Solutions       |
+|:-----:|:------------------:|:----------:|:---------------:|
+
+
+</details>
+<details>
+<summary>Solvings with difficulty "Medium"</summary>
+
+| #     | Problem            | Difficulty | Solutions       |
+|:-----:|:------------------:|:----------:|:---------------:|
+
+
+</details>
+<details>
+<summary>Solvings with difficulty "Easy"</summary>
+
+| #     | Problem            | Difficulty | Solutions       |
+|:-----:|:------------------:|:----------:|:---------------:|
+
+
+</details>
+
---
+++ scripts/buildMdFile.js
@@ -0,0 +1,83 @@
+#!/usr/bin/env node
+// @ts-check
+
+const problems = require("../problems.json");
+const path = require("path");
+const fs = require("fs").promises;
+
+const EOL = "\n"
+const GITHUB_REPO_URL = "https://github.com/Vad1mAlekseev/LeetCodeProblemSolving";
+const LEETCODE_URL = "https://leetcode.com";
+
+/**
+ * Build GitHub url from file path.
+ * @param {string} pathToFile Relative to the project root file path.
+ * @returns GitHub path url;
+ */
+function buildGithubUrl(pathToFile) {
+  return `${GITHUB_REPO_URL}/blob/main/${pathToFile}`;
+}
+
+/**
+ * Build LeetCodee url from problem id.
+ * @param {string} id Problem id.
+ */
+function buildLeetcodeUrl(id) {
+  return `${LEETCODE_URL}/problems/${id}`;
+}
+
+/**
+ * Compute difficulties count.
+ * @returns difficulties count
+ */
+function computeDifficultiesCount() {
+  return problems.reduce((acc, curr) => {
+    acc[curr.difficulty] = (acc[curr.difficulty] || 0) + 1;
+    return acc;
+  }, {});
+}
+
+/**
+ * Generate table body from problems array.
+ * @param {array} problems The problems.
+ * @returns Table body.
+ */
+function generateTableBody(problems) {
+  return problems.map((problem, idx) => `|${idx + 1}|[${problem.title}](${buildLeetcodeUrl(problem.id)})|${problem.difficulty}|${problem.solvings.map((solut) => `[${solut.title}](${solut.path})`).join(", ")}|`).join(EOL);
+}
+
+/**
+ *
+ * @param {string} summary Table header.
+ * @param {(item: problems[0]) => boolean} [difficultyPredicate] For filter problems.
+ * @returns The table.
+ */
+function generateSolvingsTable(summary, difficultyPredicate) {
+  return `<details>
+<summary>${summary}</summary>
+
+| #     | Problem            | Difficulty | Solutions       |
+|:-----:|:------------------:|:----------:|:---------------:|
+${difficultyPredicate ? generateTableBody(problems.filter(difficultyPredicate)) : generateTableBody(problems)}
+
+</details>`
+}
+
+const difficultyAmounts = computeDifficultiesCount();
+
+let mdFile = `# This repo contains my solvings leetcode problems
+
+This file was generated automatically by [buildMdFile.js](${buildGithubUrl("scripts/buildMdFile.js")}) script.
+
+Count of all problems solved: ${problems.length}
+
+${Object.keys(difficultyAmounts).map(key => `Count of solved problems with difficulty "${key}": ${difficultyAmounts[key]}`).join(EOL + EOL)}
+
+${generateSolvingsTable("All solvings")}
+${generateSolvingsTable('Solvings with difficulty "Hard"', (p => p.difficulty === "Hard"))}
+${generateSolvingsTable('Solvings with difficulty "Medium"', (p => p.difficulty === "Medium"))}
+${generateSolvingsTable('Solvings with difficulty "Easy"', (p => p.difficulty === "Easy"))}
+
+`
+
+fs.writeFile(path.join(__dirname, "..", "README.md"), mdFile);
