729da3fb6ce00fea4d25ec75e24692cf4fb8cf68
Vadim Alekseev <vad1malekseev2807@gmail.com>
2021-05-13 14:50:47 +0300

    Create queries-on-a-permutation-with-key.go

---
+++ solvings/medium/queries-on-a-permutation-with-key.go
@@ -0,0 +1,54 @@
+// 100%
+
+package solvings
+
+func processQueries(queries []int, m int) []int {
+    P := sequence(m)
+    result := make([]int, 0, m)
+    
+    
+    for idx, curr := 0, 0; idx < len(queries); idx++ {
+        curr = queries[idx]
+        seqIdx := findIdx(P, curr)
+        
+        result = append(result, seqIdx)
+        
+        moveBack(P, seqIdx)
+    }
+    
+    return result
+}
+
+func sequence(cnt int) []int {
+    result := make([]int, cnt)
+    
+    for i := 0; i < cnt; i++ {
+        result[i] = i+1
+    }
+    
+    return result
+}
+
+func moveBack(seq []int, targetIdx int) {
+    if len(seq) < targetIdx {
+        return
+    }
+    
+    tmp := seq[targetIdx]
+    
+    for idx := targetIdx-1; idx >= 0; idx-- {
+        seq[idx+1] = seq[idx]
+    }
+    
+    seq[0] = tmp
+}
+
+func findIdx(arr []int, item int) int {
+    for idx, curr := range arr {
+        if curr == item {
+            return idx
+        }
+    }
+    
+    return -1
+}
