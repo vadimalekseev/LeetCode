36422e1ca1f1adff59acb025a7e5176cc67eab46
Vadim Alekseev <vad1malekseev2807@gmail.com>
2021-05-13 15:45:02 +0300

    Create median-of-two-sorted-arrays.go

---
+++ solvings/hard/median-of-two-sorted-arrays.go
@@ -0,0 +1,9 @@
+// 98%
+
+package solvings
+
+func findMedianSortedArrays(nums1 []int, nums2 []int) float64 {
+    result := append(nums1, nums2...)
+    sort.Ints(result)
+    return float64(result[len(result)>>1] + result[(len(result)-1)>>1]) * .5
+}
