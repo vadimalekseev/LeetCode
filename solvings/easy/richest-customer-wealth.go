48080715de7023d39ca93a4c4c1385240de9eed2
Vadim Alekseev <vad1malekseev2807@gmail.com>
2021-05-17 00:12:27 +0300

    Update richest-customer-wealth.go

--- solvings/easy/richest-customer-wealth.go
+++ solvings/easy/richest-customer-wealth.go
@@ -1,27 +1,25 @@
-package solvings
-
 func maximumWealth(accounts [][]int) int {
-    m := 0
-    for _, a := range accounts {
-        m = max(sum(a), m)
-    }
-    
-    return m
+	m := 0
+	for _, a := range accounts {
+		m = max(sum(a), m)
+	}
+
+	return m
 }
 
 func sum(s []int) int {
-    sum := 0
-    for _, item := range s {
-        sum += item
-    }
-    
-    return sum
+	sum := 0
+	for _, item := range s {
+		sum += item
+	}
+
+	return sum
 }
 
 func max(a, b int) int {
-    if a > b {
-        return a
-    }
-    
-    return b
+	if a > b {
+		return a
+	}
+
+	return b
 }
