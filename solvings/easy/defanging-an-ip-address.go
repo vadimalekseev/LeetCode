0a55282f42798117742e78b78e2e1a0941ddddf6
Vadim Alekseev <vad1malekseev2807@gmail.com>
2021-05-17 00:12:43 +0300

    Update defanging-an-ip-address.go

--- solvings/easy/defanging-an-ip-address.go
+++ solvings/easy/defanging-an-ip-address.go
@@ -3,18 +3,18 @@ package solvings
 import "strings"
 
 func defangIPaddr(address string) string {
-    b := strings.Builder{}
-    
-    b.Grow(len(address) + 6)
-    
-    for _, item := range address {
-        if item == '.' {
-            b.WriteString("[.]")
-            continue
-        }
-        
-        b.WriteRune(item)
-    }
-    
-    return b.String()
+	b := strings.Builder{}
+
+	b.Grow(len(address) + 6)
+
+	for _, item := range address {
+		if item == '.' {
+			b.WriteString("[.]")
+			continue
+		}
+
+		b.WriteRune(item)
+	}
+
+	return b.String()
 }
