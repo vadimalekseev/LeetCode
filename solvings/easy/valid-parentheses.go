e0a5a12e0084e4548933cb4ac1f9c75112e3938d
Vadim Alekseev <vad1malekseev2807@gmail.com>
2021-05-16 21:57:22 +0300

    Create valid-parentheses.go

---
+++ solvings/easy/valid-parentheses.go
@@ -0,0 +1,34 @@
+package solvings
+
+func isValid(s string) bool {
+    open := []rune{'(', '{', '['}
+    clos := []rune{')','}', ']'}
+    
+    arr := []rune{}
+    for _, r := range s {
+        if ok, _ := contains(open, r); ok {
+            arr = append(arr, r)
+            continue
+        } 
+        
+        if ok, idx := contains(clos, r); ok {
+            if len(arr) == 0 || arr[len(arr)-1] != open[idx] {
+                return false
+            }
+
+            arr = arr[:len(arr)-1]
+        }
+    }
+    
+    return len(arr) == 0
+}
+
+func contains(runes []rune, item rune) (bool, int) {
+    for idx, r  := range runes {
+        if r == item {
+            return true, idx
+        }
+    }
+    
+    return false, -1
+}
