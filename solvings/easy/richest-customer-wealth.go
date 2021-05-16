c8d1ed91e50fd6b450ad5f62b2466b9b4522d198
Vadim Alekseev <vad1malekseev2807@gmail.com>
2021-05-16 23:53:10 +0300

    Create richest-customer-wealth.go

---
+++ solvings/easy/richest-customer-wealth.go
@@ -0,0 +1,25 @@
+func maximumWealth(accounts [][]int) int {
+    m := 0
+    for _, a := range accounts {
+        m = max(sum(a), m)
+    }
+    
+    return m
+}
+
+func sum(s []int) int {
+    sum := 0
+    for _, item := range s {
+        sum += item
+    }
+    
+    return sum
+}
+
+func max(a, b int) int {
+    if a > b {
+        return a
+    }
+    
+    return b
+}
